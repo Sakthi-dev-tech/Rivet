837c4698796b4edd2c2f5a16b3d2de89fa5243b7
Sakthi-dev-tech <mssakthieesvar@gmail.com>
2026-07-06 12:48:06 +0800

    Use app struct instead for ratatui

--- rivet/src/tui/main_logic.rs
+++ rivet/src/tui/main_logic.rs
@@ -1,16 +1,50 @@
 use std::io;
 
-use ratatui::{DefaultTerminal, Frame};
+use crossterm::event::{KeyCode, KeyEventKind};
+use ratatui::{
+    DefaultTerminal, Frame,
+    text::Line,
+    widgets::{Block, Paragraph},
+};
 
-pub fn tui_app(terminal: &mut DefaultTerminal) -> io::Result<()> {
-    loop {
-        terminal.draw(render)?;
-        if crossterm::event::read()?.is_key_press() {
-            break Ok(());
+struct App {
+    run_app: bool,
+}
+
+impl App {
+    fn handle_key_event(&mut self, key_event: crossterm::event::KeyEvent) -> io::Result<()> {
+        if key_event.kind == KeyEventKind::Press && key_event.code == KeyCode::Char('q') {
+            self.run_app = false;
         }
+
+        Ok(())
+    }
+
+    fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while self.run_app {
+            match crossterm::event::read()? {
+                crossterm::event::Event::Key(key_event) => self.handle_key_event(key_event)?,
+                _ => {}
+            }
+
+            terminal.draw(|frame| self.draw(frame))?;
+        }
+
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        let area = frame.area();
+
+        let block = Block::bordered().title_top(Line::from(" Rivet ").left_aligned());
+        let inner = block.inner(area);
+
+        frame.render_widget(block, area);
+        frame.render_widget(Paragraph::new("Hello World!"), inner);
     }
 }
 
-fn render (frame: &mut Frame) {
-    frame.render_widget("Hello world!", frame.area());
+pub fn tui_app(terminal: &mut DefaultTerminal) -> io::Result<()> {
+    let mut app = App { run_app: true };
+    app.run(terminal)
 }
